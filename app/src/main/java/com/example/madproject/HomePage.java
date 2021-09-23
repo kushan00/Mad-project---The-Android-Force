6627058f0b5d92f262f34a568bf1ec1a2024d248
kushan00 <kushandimantha533@gmail.com>
2021-09-23 18:49:50 +0530

    Mad app logins

---
+++ app/src/main/java/com/example/madproject/HomePage.java
@@ -0,0 +1,42 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomePage extends AppCompatActivity {
+
+    private Button clientbtn , adminbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        clientbtn = findViewById(R.id.idBTNclient);
+        adminbtn = findViewById(R.id.idBTNadmin);
+
+        clientbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        adminbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+    }
+}
